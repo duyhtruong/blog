ba3833403288bede15ef687e23549feb89a6877a
Duy Truong <duytruongh@gmail.com>
2019-01-07 21:59:54 -0800

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+ import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+
+
+//function action-creator
+ export const fetchPosts = () => async dispatch => {
+ 		const response = await jsonPlaceholder.get('/posts');
+
+		dispatch({type: 'FETCH_POSTS', payload: response.data });
+ 	}; 
+ 
+
+export const fetchUser = (id) => async dispatch => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+	dispatch({type: 'FETCH_USER', payload: response.data});
+
+};
